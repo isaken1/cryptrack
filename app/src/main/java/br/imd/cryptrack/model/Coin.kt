d88468805b8041fb3516e476fa2943ac748e1d90
Isaac Kennedy <isaackennedy@info.ufrn.br>
2019-11-25 18:09:49 -0300

    Alterações nas definições do banco

--- app/src/main/java/br/imd/cryptrack/model/Coin.kt
+++ app/src/main/java/br/imd/cryptrack/model/Coin.kt
@@ -5,9 +5,9 @@ data class Coin (
     var url: String,
     var imageUrl: String?,
     var name: String,
-    var imagePath: String
-    var symbol: String,
+    var imagePath: String?,
     var openPrice: Double?,
+    var symbol: String,
     var closePrice: Double?,
     var lowPrice: Double?,
     var highPrice: Double?,
